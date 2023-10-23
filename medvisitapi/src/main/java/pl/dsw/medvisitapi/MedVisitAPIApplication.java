542f84ed78c7f6ee6018c00ab2ef03a57661818b
Przemo <przemyslaw.baran09@gmail.com>
2023-10-23 19:04:22 +0200

    Spring Boot project initialized

---
+++ medvisitapi/src/main/java/pl/dsw/medvisitapi/MedVisitAPIApplication.java
@@ -0,0 +1,13 @@
+package pl.dsw.medvisitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedVisitAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedVisitAPIApplication.class, args);
+	}
+
+}
---
+++ medvisitapi/src/test/java/pl/dsw/medvisitapi/MedvisitapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dsw.medvisitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedvisitapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
